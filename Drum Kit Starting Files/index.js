51a5280155245047bb62f3c7370e08bdeb5e8484
Uğurcan Durna <125294764+UGURCANDURNA3301@users.noreply.github.com>
2023-06-08 12:35:47 +0300

    0.1

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,10 @@
+var numberOfDrumButtons=document.querySelectorAll(".drum").length;
+
+for( var i=0; i<document.numberOfDrumButtons;i++){
+
+document.querySelector("button")[].addEventListener("click",handleclick);
+
+function handleclick(){
+    alert("I go to clicked!");
+}}
+}
---
+++ JQery/index.js
@@ -0,0 +1,7 @@
+// $("h1").css("color", "red");
+// jquery("h1").css("color", "red");
+// $(document).ready(function () {
+//   $("h1").css("color", "blue");
+// });
+$("h1").addClass("big-title margin-50");
+$("button");
